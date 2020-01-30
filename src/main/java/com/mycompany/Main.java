5cfdc35b7cd23ccf73c26b0dad0aee5b2cc0e34b
Akmal <akmal9433@gmail.com>
2020-01-31 00:59:56 +0300

    Last data for testing

--- src/main/java/com/mycompany/Main.java
+++ src/main/java/com/mycompany/Main.java
@@ -3,19 +3,18 @@ package com.mycompany;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.regex.Pattern;
 
-import static com.mycompany.Converter.printJsonHierarchy;
+import static com.mycompany.Converter.convertFromXMlToJson;
 
 public class Main {
-    private static final String TEST_FILE_PATH = "/home/akmal/IdeaProjects/xmlJsonConverter/src/main/resources/test2.txt";
-    private static final String JSON_ELEMENT_REGEX = "\"(?<elementName>[^\"]*)\"\\s*:\\s*(?<elementValue>(\\{)|(\"[^\"]*\")|(null)|(\\d*\\.*\\d*))";
+    private static final String TEST_FILE_PATH = "/home/akmal/IdeaProjects/xmlJsonConverter/src/main/resources/test.txt";
 
-    private static final Pattern jsonElementPattern = Pattern.compile(JSON_ELEMENT_REGEX);
 
     public static void main(String[] args) throws IOException {
         String input = new String(Files.readAllBytes(Paths.get(TEST_FILE_PATH))).replaceAll("\\n+", "");
-        printJsonHierarchy("", "", input, true);
+        StringBuffer stringBuffer = new StringBuffer();
+        convertFromXMlToJson(input, stringBuffer);
+        System.out.println(stringBuffer.toString());
     }
 }
 
